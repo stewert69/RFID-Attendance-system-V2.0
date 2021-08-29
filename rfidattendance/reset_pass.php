91b560e2b1eea98a8c677c3ab367fb8efb2ea155
Stewert <43552663+stewert69@users.noreply.github.com>
2021-08-29 06:52:21 -0400

    Update reset_pass.php
    Edit this file to change sender's email address and web url!

--- rfidattendance/reset_pass.php
+++ rfidattendance/reset_pass.php
@@ -4,11 +4,11 @@ sendmail();
 function sendmail() {
 // specify recipient email address 
 $to = $_POST['email']; 
-$from = "stewert@gmail.com";
+$from = "";  // SENDER'S EMAIL ADDRESS GOES HERE!!!!!!
 $subject = "Admin Password Reset";
 $message = "<br>Visit this website to change your password. <br>";
 $message .= "<br>";
-$message .= "https://computers-by-bob.com/rfid/resetpwd.php?email=". $to . "<br>";
+$message .= "https://***************.com/resetpwd.php?email=". $to . "<br>"; // DOMAIN NAME GOES HERE !!!!!!!!!
 $headers = "From: $from\r\nReply-to: $from\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
